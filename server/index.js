3a9306e82ec779a10d621dfc8ceec0989aba7838
Zhenya Fedyai <fedyai.eugen@gmail.com>
2019-09-30 19:08:18 +0300

    Add env variables

--- server/index.js
+++ server/index.js
@@ -1,7 +1,11 @@
 const express = require("express");
 const app = express();
+const path = require("path");
+require("dotenv").config({
+  path: path.join(__dirname, "..", ".env.server")
+});
+const port = process.env.PORT;
 
-const port = 8070;
 app.listen(port, () => {
   console.log(`Listening on port ${port}`);
 });
