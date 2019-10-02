9e073cb5f80670baff89485c334c77aaa3c39ca8
Zhenya Fedyai <fedyai.eugen@gmail.com>
2019-10-02 18:44:10 +0300

    Connect mongoose lib

--- server/index.js
+++ server/index.js
@@ -5,6 +5,45 @@ require("dotenv").config({
   path: path.join(__dirname, "..", ".env.server")
 });
 const port = process.env.PORT;
+const router = require("./routes/items");
+const mongoose = require("mongoose");
+
+mongoose.connect("mongodb://127.0.0.1:27017/printbay", {
+  useNewUrlParser: true,
+  useCreateIndex: true
+});
+
+const Item = mongoose.model("Item", {
+  title: {
+    type: String
+  },
+  artist: {
+    type: String
+  },
+  image: {
+    type: String
+  },
+  year: {
+    type: Number
+  },
+  price: {
+    type: Number
+  }
+});
+
+const item = new Item({
+  title: "creation of the world"
+});
+
+(async () => {
+  try {
+    await item.save();
+  } catch (error) {
+    console.log(error);
+  }
+})();
+
+app.use("/items", router);
 
 app.listen(port, () => {
   console.log(`Listening on port ${port}`);
